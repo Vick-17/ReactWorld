82c922517145c15d45f4117d35a06c1972e45832
Vick-17 <vega.victor.0712@gmail.com>
2023-04-26 07:20:02 +0200

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+        <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis
+        doloribus magnam officiis nulla ut, temporibus possimus inventore,
+        debitis, asperiores adipisci voluptatibus! Veritatis sequi eligendi
+        labore nisi. Incidunt sunt recusandae soluta voluptatibus ut.
+        Perferendis doloremque commodi facilis esse veniam qui aliquam voluptate
+        minus officiis mollitia dolore architecto accusamus nihil soluta nemo
+        eaque numquam eos voluptatibus quis quidem sapiente, ipsa possimus
+        consequatur modi? Neque, blanditiis distinctio nostrum corrupti odio
+        facilis architecto corporis reprehenderit ipsum perspiciatis tempore,
+        accusantium non fugit aspernatur, expedita eum minus delectus numquam
+        illo minima! Unde, laborum! Nisi rerum, expedita debitis iure ipsam quam
+        facere architecto eos animi qui magnam.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam itaque
+        accusamus, repudiandae velit nostrum, quidem culpa sequi et nam dicta
+        rem dolore repellat. Tempora, doloribus suscipit in nobis deserunt error
+        illo explicabo nemo quasi architecto optio beatae nostrum nisi veniam
+        totam consequatur libero nesciunt! Facere deleniti harum fugiat nostrum
+        cumque? Atque nemo dolor voluptatum numquam vitae explicabo iusto
+        ducimus modi quibusdam itaque laborum assumenda doloremque vel
+        aspernatur natus dolore sit, est quidem, ratione cum accusantium alias!
+        Itaque, in dicta voluptatibus facere numquam aut quae et, cum, iusto
+        delectus earum cupiditate voluptates hic assumenda unde harum magni
+        magnam quos! Dolores similique quod tempore, ea consequatur repellat
+        libero officia excepturi fugit? Consectetur, soluta perspiciatis omnis
+        animi saepe velit libero neque debitis error ea ratione eveniet
+        reprehenderit vitae, at nostrum voluptate. Quas commodi amet culpa,
+        molestias inventore incidunt, soluta animi nihil eligendi neque repellat
+        a debitis modi cum maxime ipsa minima dolore quia quos qui dolorum
+        facilis. Mollitia animi illum temporibus! Quod reiciendis voluptates
+        ducimus esse eaque est quisquam iusto minima facilis non error quidem
+        animi, harum suscipit veritatis distinctio, provident vel. Voluptatem ea
+        minima sit dicta necessitatibus ducimus corporis alias repudiandae,
+        officia quaerat dolorum facere quis laudantium iusto quam consequatur
+        vero at.
+      </p>
+    </div>
+  );
+};
+
+export default About;
